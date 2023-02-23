8854b64f35f16a5f0125efc5d968e6b008abb785
guerbrown <guerin-brown@uiowa.edu>
2023-02-23 15:44:23 -0600

    Attempted to run the purl script to output a .R version of the script. This required updating. After running, I deleted the output files and attempted to replace them by runnning the new .R script file. This failed. The reason is that R in ubuntu is running 3.6.6 instead of 4.2.2. I will likely need to delete the existing R repository and replace it using a wget download.

--- script/inqcophy_PACo_Script_guerbrown_v3_20Feb2023.R
+++ script/inqcophy_PACo_Script_guerbrown_v3_20Feb2023.R
@@ -2,82 +2,92 @@
 knitr::opts_chunk$set(echo = TRUE)
 
 
+## -----------------------------------------------------------------------------
+parasitetree <- "//wsl.localhost/Ubuntu/home/guerbrown/github_local/biol-4386-course-project-guerbrown/data/PACo/inqcophy_PACo_UCECO1SynTree_v02_guerbrown_22Feb2023.nex"
+hosttree <- "//wsl.localhost/Ubuntu/home/guerbrown/github_local/biol-4386-course-project-guerbrown/data/PACo/inqcophy_PACo_CynipidTree_v02_guerbrown_22Feb2023.nex"
+assocdata <- "//wsl.localhost/Ubuntu/home/guerbrown/github_local/biol-4386-course-project-guerbrown/data/PACo/inqcophy_PACo_AssociationMatrix_guerbrown_v6_20Feb2023.csv"
+
+
 ## ----Library Chunk------------------------------------------------------------
 library(paco)
 library(ape)
 library(vegan)
 library(phytools)
 library(plotrix)
+library(smacof)
 
 
 ## ----Synergus Import and Pruning, error = T-----------------------------------
-syntree <- read.tree("~/github_local/biol-4386-course-project-guerbrown/data/PACo/inqcophy_PACo_UCECO1SynTree_guerbrown_v1_20Feb2023.nwk")
-tip1 <- c("Cer_1014_055_2A", "Diastrophus_turgidus_1322311", "Rhoophilus_loewi_1322293",
-          "Synergus_punctata_702_002_3", "Synergus_campanula_701_019_64C", "Synergus_539_067_12",
-          "Synergus_punctata_1025_023_2B", "Synergus_punctata_630_023_4A", "Synergus_punctata_994_023_1",
-          "Synergus_1486_126_1", "Synergus_1497_141_4A", "Synergus_1515_141_2", "Synergus_1589_160_36A",
-          "Synergus_walshii_362_024_1C", "Synergus_erinacei_1105_003_10A", "Synergus_villosa_B_1206_004_2",
-          "Synergus_magnus_1137_110_2", "Synergus_magnus_1140_108_7", "Synergus_1553_005_1",
-          "Synergus_oneratus_1196_023_1", "Synergus_oneratus_566_023_1", "Synergus_oneratus_52_3_3b",
-          "Synergus_oneratus_462_023_3", "Synergus_oneratus_546_019_22", "Synergus_oneratus_695_020_1",
-          "Synergus_oneratus_1105_003_5A", "Synergus_oneratus_442_002_2D", "Synergus_oneratus_1201_003_2",
-          "Synergus_oneratus_556_065_1", "Synergus_sp2_1322294", "Synergus_unknown_sp1_698_999_10C",
-          "Synergus_davisi_1076_101_13A", "Synergus_sp1_1322283", "Synergus_leviventris_1081_006_3",
-          "Synergus_leviventris_1221_006_1A", "Synergus_leviventris_1221_006_2B", "Synergus_leviventris_1164_110_3A",
-          "Synergus_leviventris_791_045_11B", "Synergus_leviventris_791_045_6A", "Synergus_leviventris_960238",
-          "Synophrus_pilulae_961203", "An_quercuspetiolicola_861_042_23B", "Neuroterus_floccosus_1552_022_6",
-          "Synergus_1059_024_6", "Synergus_1523_145_5", "Synergus_campanula_631_005_3B",
-          "Synergus_punctata_642_009_21A", "Synergus_walshii_115_1_4", "Synergus_1449_034_1",
-          "Synergus_davisi_1076_101_14A")
-p <- drop.tip(syntree, tip1, trim.internal = T)
+svg("//wsl.localhost/Ubuntu/home/guerbrown/github_local/biol-4386-course-project-guerbrown/output/inqcophy_PACo_SynTree_v01_guerbrown_23Feb2023.svg")
+
+#Import
+p <- readNexus(parasitetree)
+
+#Fixing tip labels. This is unnecessary but it's nice to make sure everything is plotting correctly
+
 tiplabel_syn <- c("Synergus_sp.6", "S._punctatus_A", "S._campanula_A", "Synergus_sp.3", "Synergus_sp.7", "Synergus_sp.8",
                    "S._walshii_A", "S._erinacei_A", "S._erinacei_B", "S._villosus", "S._magnus", "Synergus_sp.2",
-                   "S._ochreus", "S._oneratus_A", "S._oneeratus_B", "S._oneratus_C", "S._coniferae", "S._lignicola",
+                   "S._ochreus", "S._oneratus_A", "S._oneratus_B", "S._oneratus_C", "S._coniferae", "S._lignicola",
                    "S._laeviventris_A", "S._laeviventris_B", "S._laeviventris_C")
 p$tip.label <- tiplabel_syn
 
+#Rooting the phylogeny based on a version of the tree with an outgroup and plotting
+root.phylo(p, 1, resolve.root = T)
+plot.phylo(p, align.tip.label = T, use.edge.length = F, root.edge = T)
+dev.off()
 
-## ----Cynipid Import and Pruning, error =T-------------------------------------
-main2 <- read.tree("~/github_local/biol-4386-course-project-guerbrown/data/PACo/inqcophy_PACo_CynipidTree_guerbrown_v1_20Feb2023.nwk")
-tip2 <- c("Disholcaspis_sp_AZ", "Disholcaspis_endura", "Disholcaspis_quercusvirens", "Disholcaspis_cinerosa", "Andricus_brunneus", "Burnettweldia_washingtonensis", "Disholcaspis_lasia", "Cynips_plumbea", "Neutoerus_vesicula", "Atrusca_bella", "Xanthoteras_eburneum", "Andricus_quercuscalifornicus", "Unkown_hidden_white_bud", "Andricus_parmula", "Andricus_bakkeri", "Andricus_pedicellatum", "Callirhytis_pentagonal_cluster", "Callirhytis_vaccinii", "Holocynips_hartmani", "Callirhytis_quercusfutilis", "Bassettia_pallida", "Andricus_quercusutriculus", "Andricus_coortus", "Andricus_foliaformis", "Andricus_quercusfoliatus", "Andricus_sphaericus", "Andricus_quercuspetiolicola", "Callirhytis_quercusoperator", "Callirhytis_pulchra", "Callirhytis_quercuscornigera", "Callirhytis_pigra", "Callirhytis_scitula", "Zaqpatella_davisae", "Dryocosmus_quercusnotha", "Dryocosmus_cinereae", "Dryocosmus_quercuspalustris", "Zophoteras_sphaerula","Kokkocynips_imbricariae", "Callirhytis_attigua", "Amphibolips_hidalgeonsis", "Amphibolips_globulus", "Amphibolips_confluenta", "Amphibolips_quercusinanis", "Dryocosmus_rileypokei","Callirhytis_glandulus", "Andricus_chinquapin", "Atrusca_brevipennata", "Melikaiella_ostensackeni", "Melikaiella_tumifica", "Loxaulus_quercusmammula", "Andricus_operatus", "Neuroterus_saltatorius", "Neuroterus_saltatorius_texanus", "Neuroterus_quercusirregularis", "Callirhytis_exigua", "Neuroterus_distortus", "Neuroterus_pallidus", "Neuroterus_quercusverrucarum", "Neuroterus_quercusminutissimus", "Callirhytis_flavipes", "Neuroterus_flower_swelling", "Neuroterus_quercusbatatus", "Neuroterus_valhalla", "Neuroterus_fragilis", "Neuroterus_washingtonensis", "Disholcaspis_edura", "Disholandricus_lasia", "Belonocnema_fossoria", "Unknown_hidden_white_bud", "Callirhytis_quercusbatatoides", "Zapatella_davisae", "Zophoteras_sphaerula", "Amphibolips_hidalgoensis", "Andricus_opertus", "Neuroterus_vesicula", "Zopheroteras_sphaerula", "Druon_quercusflocci", "Andricus_biconicus", "Callirhytis_seminator", "unk_bud_gall_Q._macrocarpa", "Amphibolips_nubilipennis")
-h <- drop.tip(main2, tip2, trim.internal = T)
-tiplabel_gall <- c("Disholcaspis_quercusglobulus", "Disholcaspis_quercusmamma", "Acraspis_villosa", "Acraspis_quercushirta", "Acraspis_pezomachoides", "Acraspis_erinacei", "Acraspis_erinacei", "Druon_quercuslanigerum", "Cynips_conspicuus", "Philonix_fulvicollis", "Atrusca_quercuscentricola", "Belonocnema_kinseyi", "Andricus_quercusstrobilanus", "Andricus_pisiformis", "Andricus_kingii", "Callirhytis_quercuspunctata", "Callirhytis_quercusventricosa", "Dryocosmus_minusculus", "Amphibolips_quercusostensackenii", "Amphibolips_quercusjuglans", "Amphibolips_cookii")
+
+## ----Cynipid Import and Pruning, error = T------------------------------------
+svg("//wsl.localhost/Ubuntu/home/guerbrown/github_local/biol-4386-course-project-guerbrown/output/inqcophy_PACo_CynipTree_v01_guerbrown_23Feb2023.svg", width = 20, height = 10)
+
+#Import
+h <- read.nexus(hosttree)
+
+tiplabel_gall <- c("Disholcaspis_quercusglobulus", "Disholcaspis_quercusmamma", "Acraspis_villosa", "Acraspis_quercushirta", "Acraspis_pezomachoides", "Acraspis_erinacei", "Druon_ignotum", "Druon_quercuslanigerum", "Cynips_conspicuus","Philonix_fulvicollis", "Atrusca_quercuscentricola", "Belonocnema_kinseyi", "Andricus_quercusstrobilanus", "Andricus_pisiformis", "Andricus_kingii", "Callirhytis_quercuspunctata", "Callirhytis_quercusventricosa", "Dryocosmus_minusculus", "Amphibolips_quercusostensackenii", "Amphibolips_quercusjuglans", "Amphibolips_cookii")
 h$tip.label <- tiplabel_gall
 
+root.phylo(h, 1, resolve.root = T)
+plot.phylo(h, align.tip.label = T, use.edge.length = F, root.edge = T)
+dev.off()
 
-## -----------------------------------------------------------------------------
-assoc <- read.csv("~/github_local/biol-4386-course-project-guerbrown/data/PACo/inqcophy_AssociationMatrix_v6_guerbrown_20Feb2023.csv")
+
+## ----fig.height=10, fig.width=20, fig.align='center', dev='svg'---------------
+#Association matrix import
+assoc <- read.table(assocdata, header = T, sep = ",")
+#modifying the association matrix to make it readable by the program
 hp <- as.data.frame.matrix(assoc)
-cophy <- cophylo(h, p, hp)
-plot(cophy, link.type = "straight" ,link.lwd=4,
-     link.lty="solid",link.col=make.transparent("black",
-     0.2))
+
+#Removing underscores from the labels
+h$tip.label <- gsub("_", " ", h$tip.label)
+p$tip.label <- gsub("_", " ", p$tip.label)
+hp$Gallers <- gsub("_", " ", hp$Galler)
+hp$Synergus <- gsub("_", " ", hp$Synergus)
+
+#plotting the tangelgram
+svg("//wsl.localhost/Ubuntu/home/guerbrown/github_local/biol-4386-course-project-guerbrown/output/inqcophy_PACo_Tanglegram_v01_guerbrown_23Feb2023.svg", width = 20, height = 10)
+cophyloplot <- cophyloplot(h, p, hp, show.tip.label = T,
+lwd = 4, col = make.transparent("black", 0.3), font = 4,
+length.line = 0, gap = -1, space = 135, rotate = F,  align.tip.label = T, 
+use.edge.length = F, root.edge = T)+
+text(5,19.7, "Cynipidae", cex = 1.2)+
+text(168, 20.20, "Synergus", cex = 1.2)
+dev.off()
 
 
 ## -----------------------------------------------------------------------------
+#Change the two column association matrix into a binary association matrix. This is necessary to calculate principle coordinates in the statistical section.
 hp_binary <- table(hp)
 h_dist <- cophenetic(h)
 p_dist <- cophenetic(p)
+
+#Prepare PACo data and compute the result with 1,000 permutations.
 D <- prepare_paco_data(h_dist, p_dist, hp_binary)
 D <- add_pcoord(D)
 D <- PACo(D, nperm=1000, seed = NA, method= "r0", proc.warning = F)
-print(D$gof)
 
 
 ## -----------------------------------------------------------------------------
 residuals_paco(D$proc)
-
-
-## ----eval=FALSE, include=FALSE------------------------------------------------
-## h$tip.label <- gsub("_", " ", h$tip.label)
-## p$tip.label <- gsub("_", " ", p$tip.label)
-## hp$Gallers <- sub("_", " ", hp$Galler)
-## hp$Synergus <- sub("_", " ", hp$Synergus)
-## cophyloplot(h, p, hp, show.tip.label = T, use.edge.length = F,
-## lwd = weight4, col = make.transparent("black", 0.3), font = 3,
-## length.line = 0, gap = 1, space = 135, rotate = F)
-## text(5,19.7, "Cynipidae", cex = 1.2)
-## text(168, 20.20, "Synergus", cex = 1.2)
 
 
 ## -----------------------------------------------------------------------------
@@ -92,6 +102,30 @@ HostX <- unique(HP.proc$X)
 ParY <- unique(HP.proc$Yrot)
 identify(ParY[,1], ParY[,2], rownames(ParY), offset=0.3, xpd=F, cex=0.8)
 identify(HostX[,1], HostX[,2], rownames(HostX),offset=0.3, xpd=T, cex= 0.8) 
+
+
+HP.proc <- procrustes(D$H_PCo, D$P_PCo)
+HP.proc
+summary(HP.proc)
+plot(HP.proc, pch = 21)
+
+## Configuration plots; Procrustes plots.
+plot(D$H_PCo, main = "Host")   ## MDS plot East Germany
+plot(D$P_PCo, main = "Parasite")   ## MDS plot West Germany
+
+## Procrustes configurations (X and Yhat)
+plot(HP.proc, ylim = c(-1, 1),  col.X = "cadetblue", col.Yhat = "brown", pch = 19, 
+    legend = list(pos = "topleft", labels = c("Host", "Parasite"))) 
+    
+## Procrustes transformations (Y and Yhat)
+plot(HP.proc, plot.type = "transplot", length = 0.05, ylim = c(-1,1), 
+     legend = list(pos = "bottomright", 
+     labels = c("Parasite (untransformed)", "Host (transformed)")))
+
+
+A.normalized.df <- as.data.frame(t(D$P_PCo))
+B.transformed.df <- as.data.frame(t(D$H_PCo))
+data.df <- rbind(D$H_PCo, D$P_PCo, A.normalized.df, B.transformed.df)
 
 
 ## -----------------------------------------------------------------------------

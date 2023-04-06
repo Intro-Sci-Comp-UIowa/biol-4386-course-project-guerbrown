99ca10a3c641f60708dbaab4582a0462deaf9557
guerbrown <guerbrown@LAPTOP-NQJ9JKGS.localdomain>
2023-04-06 13:18:02 -0500

    Success! I managed to get the python script for removing white backgrounds of .svg images working. I also modified the PACo script to automatically plot red lines over the tanglegram for significant signals of cospeciation and replicated this color in the residuals barplot by recoloring the astericks. Finally, I made a Master Script file in the analysis folder that should be able to process all script automatically. The only thing left is to create a script to automatically resize images and combine them into a full image. I will likely use python for this.

--- script/TraitModeling_Script/inqcophy_TraitModeling_Dependencies_guerbrown.R
+++ script/TraitModeling_Script/inqcophy_TraitModeling_Dependencies_guerbrown.R
@@ -1,11 +1,16 @@
 #Use this script to download any packages used in the analysis
 
-#install.packages("geiger")
-#install.packages("paco")
-#install.packages("ape")
-#install.packages("vegan")
-#install.packages("phytools")
-#install.packages("plotrix")
-#install.packages("svglite")
-#install.packages("knitr")
+install.packages("geiger")
+install.packages("paco")
+install.packages("ape")
+install.packages("vegan")
+install.packages("phytools")
+install.packages("plotrix")
+install.packages("svglite")
+install.packages("knitr")
+#If corHMM will not install run this:
+#sudo apt-get install libgmp-dev libmpfr-dev
+#install.packages("gmp")
+#install.packages("Rmpfr")
+#install.packages("corHMM")
 install.packages("corHMM")

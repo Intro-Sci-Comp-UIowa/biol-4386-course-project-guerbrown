541c7ae566eb84b9b2474387d1855e504cbb4839
guerbrown <guerin-brown@uiowa.edu>
2023-04-18 08:23:47 -0500

    Image modification script has stopped working. I beleive this is due to the inkscape package. It requires a virtual server to access and modify graphics. I may have accidentally initiated this last time but cannot remember how. I'll have to find away to automate the formation of the virutal server within the script itself. Shouldn't be too difficult.

--- script/Image_Modification_Script/inqcophy_ImageModification_Script.py
+++ script/Image_Modification_Script/inqcophy_ImageModification_Script.py
@@ -2,17 +2,15 @@ import os
 
 # Specify the directory containing the SVG images
 input_dir = '/home/guerbrown/github_local/biol-4386-course-project-guerbrown/output'
+output_dir = '~/github_local/biol-4386-course-project-guerbrown/final_images'
 
 # Loop through each SVG file in the input directory
 for filename in os.listdir(input_dir):
     if filename.endswith('.svg'):
+
         # Define the input and output filenames
         input_file = os.path.join(input_dir, filename)
-        output_file = os.path.join(input_dir, 'no_bg_' + filename)
-        
-        # Print the output file path for debugging
-        print(f'Saving output to {output_file}')
+        output_file = os.path.join(output_dir, filename + 'no_bg_')
         
         # Call Inkscape to remove the background
-        os.system(f'inkscape --export-background-opacity=0 --export-png={output_file} {input_file}')
-
+        os.system(f'inkscape --export-background-opacity=0 --export-svg={output_file} {input_file}')

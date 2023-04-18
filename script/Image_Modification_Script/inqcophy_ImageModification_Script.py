4c5a1c588e4fd8a241b894bbb19d79bdd60c873e
guerbrown <guerin-brown@uiowa.edu>
2023-04-18 13:33:04 -0500

    Updates to the image modification scripts. I managed to get the code working again. I tried using xvfb to initialize a virutal server and the subprocess import to initialize a "false" version of these (essentially forcing the inkscape package to run like it would on the command line of a virtual server but falsified so it runs in script). This didn't work quite as expected. So, I used cairosvg instead. This allows me to convert SVG to PNG and remove the backgrounds directly. It is likely less efficient than inkscape but it works.

--- script/Image_Modification_Script/inqcophy_ImageModification_Script.py
+++ script/Image_Modification_Script/inqcophy_ImageModification_Script.py
@@ -1,8 +1,13 @@
 import os
+import cairosvg
+from xml.etree import ElementTree
 
 # Specify the directory containing the SVG images
 input_dir = '/home/guerbrown/github_local/biol-4386-course-project-guerbrown/output'
-output_dir = '~/github_local/biol-4386-course-project-guerbrown/final_images'
+output_dir = os.path.expanduser('~/github_local/biol-4386-course-project-guerbrown/final_images')
+
+# Make sure output directory exists
+os.makedirs(output_dir, exist_ok=True)
 
 # Loop through each SVG file in the input directory
 for filename in os.listdir(input_dir):
@@ -10,7 +15,14 @@ for filename in os.listdir(input_dir):
 
         # Define the input and output filenames
         input_file = os.path.join(input_dir, filename)
-        output_file = os.path.join(output_dir, filename + 'no_bg_')
-        
-        # Call Inkscape to remove the background
-        os.system(f'inkscape --export-background-opacity=0 --export-svg={output_file} {input_file}')
+        output_file = os.path.join(output_dir, filename.replace('.svg', '_no_bg_.png'))
+
+        # Load the SVG file and remove the background
+        with open(input_file, 'r') as svg_file:
+            svg_data = svg_file.read()
+            tree = ElementTree.fromstring(svg_data)
+            tree.attrib['style'] = 'background-color:none'
+
+        # Save the modified SVG as a PNG
+        cairosvg.svg2png(bytestring=ElementTree.tostring(tree), write_to=output_file)
+

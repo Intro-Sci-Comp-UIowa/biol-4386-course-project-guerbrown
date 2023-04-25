97d31f83454c07b43ac9f9107caa2e38e65cf722
guerbrown <guerin-brown@uiowa.edu>
2023-04-25 10:50:45 -0500

    Final Commit. Removed one last piece of unused code.

--- script/PACo_Script/inqcophy_PACo_Dependencies_guerbrown_v1_20Feb2023.R
+++
@@ -1,9 +0,0 @@
-#Use this script to download any packages used in the analysis
-
-install.packages("paco")
-install.packages("ape")
-install.packages("vegan")
-install.packages("phytools")
-install.packages("plotrix")
-install.packages("svglite")
-install.packages("knitr")
